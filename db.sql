acf96d896d7bf0f9d9141bf4f9a5731cc56a8a57
Jahirsf21 <deislhersanchezfunez@gmail.com>
2026-06-13 10:18:35 -0600

    add: esquema base de datos

---
+++ db.sql
@@ -0,0 +1,35 @@
+CREATE DATABASE TIENDA;
+USE TIENDA;
+
+CREATE TABLE Producto(
+    IdProducto INT PRIMARY KEY,
+    Nombre VARCHAR(100),
+    Precio DECIMAL(10,2),
+    Stock INT
+);
+
+CREATE TABLE AuditoriaPrecio(
+    IdAuditoria INT IDENTITY PRIMARY KEY,
+    IdProducto INT,
+    PrecioAnterior DECIMAL(10,2),
+    PrecioNuevo DECIMAL(10,2),
+    FechaCambio DATETIME,
+    UsuarioBD VARCHAR(100)
+);
+
+CREATE TABLE AuditoriaEliminacion(
+    IdAuditoria INT IDENTITY PRIMARY KEY,
+    IdProducto INT,
+    Nombre VARCHAR(100),
+    Precio DECIMAL(10,2),
+    FechaEliminacion DATETIME,
+    UsuarioBD VARCHAR(100)
+);
+
+CREATE TABLE BitacoraDDL(
+    IdEvento INT IDENTITY PRIMARY KEY,
+    EventoDDL VARCHAR(100),
+    Objeto VARCHAR(200),
+    FechaEvento DATETIME,
+    UsuarioBD VARCHAR(100)
+);
